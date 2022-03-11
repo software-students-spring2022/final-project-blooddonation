bb06b50ac73cf3ee7a89620eda3fe151a75bc810
keyaneealexandre <ka1974@nyu.edu>
2022-03-11 01:56:24 -0500

    Almost done with eligibility page, added basic eligibility info for each type of donation

--- front-end/src/Eligibility.js
+++ front-end/src/Eligibility.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import './Eligibility.css'
 import { EligibilityData} from './components/EligibilityData';
-import { Card, CardContent, CardMedia, Box, Typography as t} from '@mui/material';
+import { Card, CardContent, CardMedia, Box, Typography, Grid} from '@mui/material';
 
 
 /**
@@ -18,8 +18,12 @@ const Eligibility = props => {
           <div className="eligibilityleft-link">
             <Link to="/FAQ" className="backlink">Back to FAQ</Link>
           </div>
+          <div>
+            <h1 className='title'>Eligibility</h1>
+          </div>
         </nav>
 
+        <div className='site-container'><a href="/finddonationsite" className='findSite-link'>Find a Donation Site</a></div>
         <div className="container">
           <a href="#wholeblood" className='side-link'>Whole Blood Donation</a>
           <a href="#powerred" className='side-link'>Power Red Donation (Double Red Cell)</a>
@@ -27,48 +31,190 @@ const Eligibility = props => {
           <a href="#plasma" className='side-link'>Plasma Donation</a>
         </div>
 
-        {EligibilityData.map((item)=>{
-          return(
-              item.section === "Whole Blood Donation" ?
-              <div class="main" id="wholeblood">
-                <p>{item.image}</p>
-
-              <Card sx={{ display: 'flex', minWidth: 200, height: 400}}>
-                  <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>  
-                      {item.image && 
-                      <CardMedia component = "img" sx= {{ width: 100, borderRadius: '50%'}}image = {item.image}/>
-                      }
-                  </Box>
-                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                      <CardContent>
-                          <h2 className='sectionheader'>{item.name}</h2> 
-                          <t component = "div" variant = "h1"> {item.points}<br /></t>
-                          <t component = "div" variant = "h1"> {item.links}</t>
-                      </CardContent>
-                  </Box>
-              </Card>
-            </div>
-
-            :null
-          );
-        })
-        }
+        <Grid container direction="column" justifyContent="center" alignItems="center"> 
         
+        
+        
+      
+          {EligibilityData.map((item)=>{
+            return(
+                item.section === "Whole Blood Donation" ?
+                <div class="main" id="wholeblood">
 
-        <div class="main" id="powerred">
-          <h2 className='sectionheader'>Power Red Donation (Double Red Cell)</h2>
-          <a href="#platelet">Click Me to Smooth Scroll to below</a>
-        </div>
+                  <Grid item>
+                    <Card sx={{ display: 'flex', minWidth: 200, width: 1100, height: 515}}>
+                        <Box sx={{ display: 'flex', alignItems: 'left'}}>  
+                            {item.image && 
+                            <CardMedia component = "img" sx= {{ width: 580, height: 515}}image = {item.image}/>
+                            }
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent>
 
-        <div class="main" id="platelet">
-          <h2 className='sectionheader'>Platelet Donation</h2>
-          <a href="#plasma">Click Me to Smooth Scroll to below</a>
-        </div>
 
-        <div class="main" id="plasma">
-          <h2 className='sectionheader'>AB Elite Plasma Donation</h2>
-          <a href="#wholeblood">Click Me to Smooth Scroll to top</a>
-        </div>
+                                <Typography component = "div" variant = "h4" sx={{ pr: 15, pb: 1}}> {item.section}<br /></Typography>
+
+                                
+                               
+                                  {item.points.map((point)=>{
+                                    return(
+                                      <Typography sx={{ pl: 0, pt: 2, pb: 0, color: 'red', fontSize: 30, display: 'flex',  flexDirection: 'row'}}>•  <Typography sx={{ pt: 1, pl: 1, pb: 0, color: 'black'}}>{point}</Typography></Typography>
+                                    );
+                                  })}
+                                  
+                       
+                                {item.links.map((link)=>{
+                                    return(
+                                      <Typography component = "div" variant = "subtitle1" sx={{ color: 'red', pt: 3, whiteSpace: 'nowrap'}}> {link}</Typography>
+                                    );
+                                })}
+
+                                
+                            </CardContent>
+                        </Box>
+                    </Card>
+                  </Grid>
+                </div>
+
+                :item.section === "Power Red Donation (Double Red Cell)" ?
+                 
+                 <Grid item>
+
+                  <div class="main" id="powerred">
+
+                    <Card sx={{ display: 'flex', minWidth: 200, width: 1100, height: 515}}>
+                        <Box sx={{ display: 'flex', alignItems: 'left'}}>  
+                            {item.image && 
+                            <CardMedia component = "img" sx= {{ width: 580, height: 515}}image = {item.image}/>
+                            }
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent>
+
+
+                            <Typography component = "div" variant = "h4" sx={{ pr: 15, pb: 1}}> {item.section}<br /></Typography>
+
+                                
+                               
+                            {item.points.map((point)=>{
+                              return(
+                                <Typography sx={{ pl: 0, pt: 2, pb: 0, color: 'red', fontSize: 30, display: 'flex',  flexDirection: 'row'}}>•  <Typography sx={{ pt: 1, pl: 1, pb: 0, color: 'black'}}>{point}</Typography></Typography>
+                              );
+                            })}
+
+
+                            {item.links.map((link)=>{
+                              return(
+                                <Typography component = "div" variant = "subtitle1" sx={{ color: 'red', pt: 3, whiteSpace: 'nowrap'}}> {link}</Typography>
+                              );
+                            })}
+
+                                
+                            </CardContent>
+                        </Box>
+                    </Card>
+                  </div>
+                 </Grid>
+
+                :item.section === "Platelet Donation" ?
+
+                <div class="main" id="platelet">
+
+                  <Grid item>
+                    <Card sx={{ display: 'flex', minWidth: 200, width: 1100, height: 515}}>
+                        <Box sx={{ display: 'flex', alignItems: 'left'}}>  
+                            {item.image && 
+                            <CardMedia component = "img" sx= {{ width: 580, height: 515}}image = {item.image}/>
+                            }
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent>
+
+
+                                <Typography component = "div" variant = "h4" sx={{ pr: 15, pb: 1}}> {item.section}<br /></Typography>
+
+                                
+                              
+                                  {item.points.map((point)=>{
+                                    return(
+                                      <Typography sx={{ pl: 0, pt: 2, pb: 0, color: 'red', fontSize: 30, display: 'flex',  flexDirection: 'row'}}>•  <Typography sx={{ pt: 1, pl: 1, pb: 0, color: 'black'}}>{point}</Typography></Typography>
+                                    );
+                                  })}
+                                  
+                      
+                                {item.links.map((link)=>{
+                                    return(
+                                      <Typography component = "div" variant = "subtitle1" sx={{ color: 'red', pt: 3, whiteSpace: 'nowrap'}}> {link}</Typography>
+                                    );
+                                })}
+
+                                
+                            </CardContent>
+                        </Box>
+                    </Card>
+                  </Grid>
+                </div>
+
+
+                
+                :item.section === "AB Elite Plasma Donation" ?
+
+                  <div class="main" id="plasma">
+
+                    <Grid item>
+                      <Card sx={{ display: 'flex', minWidth: 200, width: 1100, height: 515}}>
+                          <Box sx={{ display: 'flex', alignItems: 'left'}}>  
+                              {item.image && 
+                              <CardMedia component = "img" sx= {{ width: 580, height: 700}}image = {item.image}/>
+                              }
+                          </Box>
+                          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                              <CardContent>
+
+
+                                  <Typography component = "div" variant = "h4" sx={{ pr: 15, pb: 1}}> {item.section}<br /></Typography>
+
+                                  
+                                
+                                    {item.points.map((point)=>{
+                                      return(
+                                        <Typography sx={{ pl: 0, pt: 2, pb: 0, color: 'red', fontSize: 30, display: 'flex',  flexDirection: 'row'}}>•  <Typography sx={{ pt: 1, pl: 1, pb: 0, color: 'black'}}>{point}</Typography></Typography>
+                                      );
+                                    })}
+                                    
+                        
+                                  {item.links.map((link)=>{
+                                      return(
+                                        <Typography component = "div" variant = "subtitle1" sx={{ color: 'red', pt: 3, whiteSpace: 'nowrap'}}> {link}</Typography>
+                                      );
+                                  })}
+
+                                  
+                              </CardContent>
+                          </Box>
+                      </Card>
+                    </Grid>
+                  </div>
+
+          
+              
+                :null
+              );
+            })
+          }
+              
+              
+
+        
+        
+        
+        
+        
+        
+        </Grid>
+           
+
+       
 
       </body>
 
