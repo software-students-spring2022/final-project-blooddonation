d2cfa5929f79ecf2373f08dba45deee208cda16f
keyaneealexandre <ka1974@nyu.edu>
2022-03-11 21:04:51 -0500

    completed Eligibility page and fixed header

---
+++ front-end/src/components/LifestyleData.js
@@ -0,0 +1,58 @@
+export const LifestyleData = [
+
+    {
+      question: 'Age',
+    },
+
+    {
+      question: 'Donor Deferral for Men Who Have Had Sex With Men (MSM)',
+      answer: [ { subHeading: "",
+                  points: [ "First-time male donors may be eligible to donate blood if they have not had sex with another man in more than 3 months.. All additional blood donation eligibility criteria will apply. ",
+                            "Donors who were previously deferred under the prior MSM policy will be evaluated for reinstatement. It is important to understand that the donor reinstatement process involves potentially thousands of donors, and it will take time."
+                    ]
+                }
+      ]
+    },
+
+    {
+      question: 'Intravenous Drug Use',
+      answer: [ { subHeading: "",
+                  points: ["Wait 3 months after using IV drugs that were not prescribed by a physician."]
+                }
+      ]
+    },
+
+
+    {
+        question: 'Pregnancy, Nursing',
+        answer: [ { subHeading: "",
+                    points: ["Those who are pregnant are not eligible to donate. Wait 6 weeks after giving birth."]
+                  }
+        ]
+    },
+
+
+    {
+        question: 'Tattoo',
+        answer: [ { subHeading: "",
+                    points: [ "Wait 3 months after a tattoo if the tattoo was applied in a state that does not regulate tattoo facilities. Currently, the only states that DO NOT regulate tattoo facilities are: District of Columbia, Georgia, Idaho, Maryland, Massachusetts, New Hampshire, New York, Pennsylvania, Utah and Wyoming.",
+                                "A tattoo is acceptable if the tattoo was applied by a state-regulated entity using sterile needles and ink that is not reused. Cosmetic tattoos (including microblading of eyebrows only) applied in a licensed establishment in a regulated state using sterile needles and ink that is not reused is acceptable. You should discuss your particular situation with the health historian at the time of donation."
+                    ]
+                  }
+        ]
+    },
+
+    {
+        question: 'Piercing (ears, body), Electrolysis',
+        answer: [ { subHeading: "",
+                    points: [ "You are eligible as long as the instruments used were single-use equipment and disposable (which means both the gun and the earring cassette were disposable). Wait 3 months if a piercing was performed using a reusable gun or any reusable instrument.",
+                              "Wait 3 months if there is any question whether or not the instruments used were single-use equipment. "
+                    ]
+                  }
+        ]
+    },
+
+
+    
+
+  ];
