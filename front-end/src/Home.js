acb4bca20c6f07614de2becf9d463de4eeccabb3
rkindangen <rmk461@nyu.edu>
2022-03-19 08:50:54 -0700

    add content and styling changes to home

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,8 +1,9 @@
 import './styles/Home.css'
-import { Container , Grid, Item} from '@mui/material';
+import { Button, Grid, Item} from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import {national_blood_crisis_text} from './text/HomePageText';
+import ReactFullpage from "@fullpage/react-fullpage";
 
 /**
  * A React component that represents the Home page of the app.
@@ -20,21 +21,71 @@ const Home = props => {
   
   return (
     <>
-      <body className='home-body'>
-        <Container>
-          <h1>National Blood Crisis</h1>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Item>{national_blood_crisis_text}</Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>{national_blood_crisis_text}</Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>{national_blood_crisis_text}</Item>
-          </Grid>
-        </Grid>
-        </Container>
+    <body className='home-body'>
+      <ReactFullpage
+      scrollingSpeed = {1000} 
+      sectionsColor={["orange"]}
+      paddingBottom = "10px"
+
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <div className="slide">
+                    <h3>Image</h3>
+                  </div>
+                  <div className="slide">
+                    <h3>Image</h3>
+                  </div>
+                  <div className="slide">
+                    <h3>Image</h3>
+                  </div>
+            </div>
+            <div className="section">
+            <Grid container spacing={2} sx = {{mb: 9}}>
+              <Grid item xs={12}>
+                <h1>National Blood Crisis</h1>
+                <h3>{national_blood_crisis_text}</h3>
+              </Grid>
+              <Grid item xs={4}>
+                <h1>Picture</h1>
+                <Button>Read More</Button>
+              </Grid>
+              <Grid item xs={4}>
+                <h1>Picture</h1>
+                <Button>Read More</Button>
+              </Grid>
+              <Grid item xs={4}>
+                <h1>Picture</h1>
+                <Button>Read More</Button>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} sx = {{mb: 9}}>
+              <Grid item xs={12}>
+                <h1>How Blood Donations Help</h1>
+              </Grid>
+              <Grid item xs={8}>
+                <Item>{national_blood_crisis_text}</Item>
+              </Grid>
+              <Grid item xs={4}>
+                <h1>Video</h1>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} sx = {{mb: 9}}>
+              <Grid item xs={12}>
+                <h1>Why Host a Blood Drive</h1>
+              </Grid>
+              <Grid item xs={12}>
+                <Item>{national_blood_crisis_text}</Item>
+                <Button>Find Out How to Host a Blood Drive</Button>
+              </Grid>
+            </Grid>
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
+
       </body>
     </>
   )
