16cbb3f862d634d84af304eb1b71431c813f4070
ELENA0206 <qianwang0206@gmail.com>
2022-04-02 20:06:15 +0300

    added login and registration tests

--- back-end/server.js
+++ back-end/server.js
@@ -2,16 +2,47 @@
 /* eslint-disable no-console */
 const server = require('./app');
 // load up the web server
-const port = 3000; // the port to listen to for incoming requests
+const port = process.env.port || 3000; // the port to listen to for incoming requests
 // call express's listen function to start listening to the port
 const listener = server.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+  //console.log(`Server running on port: ${port}`);
 });
 // a function to stop listening to the port
 const close = () => {
   listener.close();
 };
 
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome' });
+});
+
+server.post('/register', (req, res) => {
+  const user = {
+    firstName: "Rachel",
+    lastName: "Kindagen",
+    email: "rmk461@nyu.edu",
+    password: "helloworld123",
+    age: 21,
+    eligible: [],
+    loggedIn: false,
+  }
+  res.status(200).json(user);
+});
+
+server.post('/login', (req, res) => {
+  const data = {
+    email: 'rmk461@nyu.edu',
+    password: 'helloworld123'
+  }
+
+  if (req.body.email === data.email && req.body.password === data.password) {
+    res.status(200).json(data);
+  }
+  res.status(401).json({ message: 'Unauthorized' });
+
+});
+
 module.exports = {
   close,
 };
+module.exports = server;
