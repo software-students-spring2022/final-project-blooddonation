27047067d23d6e59a14ea780a2d11fa0dc1cc589
keyaneealexandre <ka1974@nyu.edu>
2022-04-06 13:10:43 -0400

    set up database in mongoDB Atlas and fix app and server, they needed to be switched

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,77 @@
-// import and instantiate express
-const express = require('express'); // CommonJS import style!
+#!/usr/bin/env node
+/* eslint-disable no-console */
+require('dotenv').config({ silent: true });
+// load up the web server
 
-const app = express(); // instantiate an Express object
-// we will put some server logic here later...
-// export the express app we created to make it available to other modules
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json()); // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })); // decode url-encoded incoming POST data
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+mongoose
+  .connect(`${process.env.DB_CONNECTION_STRING}`)
+  .then((data) => console.log(`Connected to MongoDB`))
+  .catch((err) => console.error(`Failed to connect to MongoDB: ${err}`));
+
+const User = require('./models/User');
+
+// Passport Local Strategy
+passport.use(
+  new LocalStrategy(
+    // function of username, password, done(callback)
+    (username, password, done) => {
+      // look for the user data
+      User.findOne({ username }, (err, user) => {
+        // if there is an error
+        if (err) {
+          return done(err);
+        }
+        // if user doesn't exist
+        if (!user) {
+          return done(null, false, { message: 'User not found.' });
+        }
+        // if the password isn't correct
+        if (!user.verifyPassword(password)) {
+          return done(null, false, {
+            message: 'Invalid password.',
+          });
+        }
+        // if the user is properly authenticated
+        return done(null, user);
+      });
+    }
+  )
+);
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
+
+app.post(
+  '/login',
+  passport.authenticate('local', { failureRedirect: '/createaccount' }),
+  (req, res) => {
+    console.log(req.user);
+    // to do: redirect to unique dashboard / profile page
+    res.redirect('./dashboard');
+  }
+);
+
+app.post('/createaccount', (req, res) => {
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  let password = req.body.password;
+});
+
 module.exports = app;
+
+// module.exports = {
+//   close,
+// };
--- back-end/app.test.js
+++ back-end/app.test.js
@@ -1,48 +1,38 @@
-
 const { expect } = require('chai');
 const request = require('supertest');
-const server = require('./server')
+const app = require('./app');
 
+describe('Test user login and registration', () => {
+  it('POST /login ------> test user login', () =>
+    request(app)
+      .post('/login')
+      .send({
+        email: 'rmk461@nyu.edu',
+        password: 'helloworld123',
+      })
+      .expect(200)
+      .then((response) => {
+        expect(response.body.email).equal('rmk461@nyu.edu');
+      }));
 
-describe('Test user login and registration',()=>{
+  it('POST /register -----> test user registration', () => {
+    const user = {
+      firstName: 'Rachel',
+      lastName: 'Kindagen',
+      email: 'rmk461@nyu.edu',
+      password: 'helloworld123',
+      age: 21,
+      eligible: [],
+      loggedIn: false,
+    };
 
-    it('POST /login ------> test user login', () => {
-
-        return request(server)
-            .post('/login')
-            .send({
-                email: 'rmk461@nyu.edu',
-                password: 'helloworld123'
-            })
-            .expect(200)
-            .then((response) => {
-                expect(response.body.email).equal("rmk461@nyu.edu")
-            })
-    })
-    
-    
-    it('POST /register -----> test user registration', () => {
-    
-        const user = {
-            firstName: "Rachel",
-            lastName: "Kindagen",
-            email: "rmk461@nyu.edu",
-            password: "helloworld123",
-            age: 21,
-            eligible: [],
-            loggedIn: false,
-        }
-    
-        return request(server)
-            .post('/register')
-            .send(user)
-            .expect(200)
-            .then((response) => {
-                expect(response.body.email).equal("rmk461@nyu.edu")
-                expect(response.body.age).above(20)
-            })
-    })
-
-})
-
-
+    return request(app)
+      .post('/register')
+      .send(user)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.email).equal('rmk461@nyu.edu');
+        expect(response.body.age).above(20);
+      });
+  });
+});
