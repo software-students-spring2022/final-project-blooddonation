c317230b54bc7413a513a20ec7c0a2cd41123c3e
shrankhla1234 <75555215+shrankhla1234@users.noreply.github.com>
2022-04-13 12:03:03 -0400

    commit

--- front-end/src/FindDonationSite.js
+++ front-end/src/FindDonationSite.js
@@ -1,40 +1,54 @@
 import './styles/FindDonationSite.css'
 
-/**
- * A React component that represents the Home page of the app.
- * @param {*} param0 an object holding any props passed to this component from its parent component
- * @returns The contents of this component, in JSX form.
- */
 const FindDonationSite = props => {
-  return (
-    <>
-      <body className='find-body'>
-        <h1>Hello and welcome!</h1>
-        <p>This is our blood donation Find Donation Site Page</p>
-      </body>
-      <link href="https://donate.nybc.org/donor/schedules/center/96" rel="stylesheet"></link>
+    return (
 
-      <link href="https://donate.nybc.org/donor/schedules/center/79" rel="stylesheet"></link>
+    } 
+    var link;{
+      switch (e.target.feature.properties.name) {
+        case "Donation site within NJ/NY"
+             
+      <ul /><><main></main><body className='find-body'>
 
-      <link href="https://donate.nybc.org/donor/schedules/center/106" rel="stylesheet"></link>
+            <h1>Hello and welcome!</h1>
 
-      <link href="https://donate.nybc.org/donor/schedules/center/38" rel="stylesheet"></link>
+            <p>This is our blood donation Find Donation Site Page</p>
 
-      <link href="https://donate.nybc.org/donor/schedules/center/93" rel="stylesheet"></link>
+          </body><link
+            href="https://donate.nybc.org/donor/schedules/center/96"
+            rel="stylesheet"></link><link
 
-      <link href="https://donate.nybc.org/donor/schedules/center/31" rel="stylesheet"></link>
+              href="https://donate.nybc.org/donor/schedules/center/79"
+              rel="stylesheet"></link><link
 
-      <link href="https://donate.nybc.org/donor/schedules/center/49" rel="stylesheet"></link>
+                href="https://donate.nybc.org/donor/schedules/center/106"
+                rel="stylesheet"></link><link
 
-    <h1>SearchableMap</h1>
-    <p>
-    
-    </p>
+                  href="https://donate.nybc.org/donor/schedules/center/38"
+                  rel="stylesheet"></link><link
 
-    </>
-  )
-}
+                    href="https://donate.nybc.org/donor/schedules/center/93"
+                    rel="stylesheet"></link><link
 
+                      href="https://donate.nybc.org/donor/schedules/center/31"
+                      rel="stylesheet"></link><link
 
-// make this component available to be imported into any other file
-export default FindDonationSite
+                        href="https://donate.nybc.org/donor/schedules/center/49"
+                        rel="stylesheet"></link><h1>SearchableMap</h1><p>
+
+              {'}'}
+
+              return (
+              <div>
+                click: DonationSite;
+
+              </div>
+
+              });
+
+            </script></>
+  </body>
+</html>
+            
+
+}     
